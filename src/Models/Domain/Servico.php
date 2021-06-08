4e77af247104dba947ba5ddc6adbf5e27b8c54a0
Lucas Corte <corte1994.lc@gmail.com>
2021-06-08 19:36:20 -0300

    implementação da classe Servico.php

---
+++ src/Models/Domain/Servico.php
@@ -0,0 +1,73 @@
+<?php
+
+
+namespace App\Models\Domain;
+
+
+class Servico
+{
+    private int $codigo;
+    private string $descricao;
+    private float $preco;
+
+    /**
+     * Servico constructor.
+     * @param int $codigo
+     * @param string $descricao
+     * @param float $preco
+     */
+    public function __construct(int $codigo, string $descricao, float $preco)
+    {
+        $this->codigo = $codigo;
+        $this->descricao = $descricao;
+        $this->preco = $preco;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCodigo(): int
+    {
+        return $this->codigo;
+    }
+
+    /**
+     * @param int $codigo
+     */
+    public function setCodigo(int $codigo): void
+    {
+        $this->codigo = $codigo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescricao(): string
+    {
+        return $this->descricao;
+    }
+
+    /**
+     * @param string $descricao
+     */
+    public function setDescricao(string $descricao): void
+    {
+        $this->descricao = $descricao;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPreco(): float
+    {
+        return $this->preco;
+    }
+
+    /**
+     * @param float $preco
+     */
+    public function setPreco(float $preco): void
+    {
+        $this->preco = $preco;
+    }
+}
