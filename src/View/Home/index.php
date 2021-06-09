5b1b846127839e61241948aa4b9b5d2cbbcd59a6
Lucas Corte <corte1994.lc@gmail.com>
2021-06-08 21:44:32 -0300

    implementacao dos controllers

---
+++ src/View/Home/index.php
@@ -0,0 +1,7 @@
+<?php require '../src/View/cabecalho.php'; ?>
+
+    <div class="mt-1 card-header">
+        <h1 class="display-4 mb-2">Bem vindo!</h1>
+    </div>
+
+<?php require '../src/View/rodape.php'; ?>
